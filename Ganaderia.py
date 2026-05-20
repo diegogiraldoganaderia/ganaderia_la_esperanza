e303904f443a6575aab78523f11608f6ecc99bcb
diegogiraldoganaderia <diegogiraldo1304@gmail.com>
2026-05-20 17:34:51 -0500

    Add files via upload

--- Ganaderia.py
+++ Ganaderia.py
@@ -319,17 +319,18 @@ class Buscador:
 def registros(registro):
 
    options = Options()
-
-   options.add_argument("--headless")
+   options.add_argument("--headless=new")
    options.add_argument("--no-sandbox")
    options.add_argument("--disable-dev-shm-usage")
    options.add_argument("--disable-gpu")
    options.add_argument("--window-size=1920,1080")
+   options.add_argument("--remote-debugging-port=9222")
 
    from selenium.webdriver.chrome.service import Service
    options.binary_location = "/usr/bin/chromium-browser"
 
    service = Service("/usr/bin/chromedriver")
+   
 
    driver = webdriver.Chrome(service=service,options=options)
 
