82a23442d4cf0882eb0fc12e52397449b1a3b190
diegogiraldoganaderia <diegogiraldo1304@gmail.com>
2026-05-20 17:41:11 -0500

    Add files via upload

--- Ganaderia.py
+++ Ganaderia.py
@@ -25,13 +25,11 @@ options.add_argument("--window-size=1920,1080")
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
-from selenium.webdriver.chrome.service import Service
 
 
 df_Animal=pd.read_excel("DATA.xlsx",sheet_name=0)
@@ -318,21 +316,22 @@ class Buscador:
 
 def registros(registro):
 
+  def registros(registro):
+
    options = Options()
+
+   options.binary_location = "/usr/bin/chromium"
+
    options.add_argument("--headless=new")
    options.add_argument("--no-sandbox")
    options.add_argument("--disable-dev-shm-usage")
-   options.add_argument("--disable-gpu")
-   options.add_argument("--window-size=1920,1080")
-   options.add_argument("--remote-debugging-port=9222")
-
-   from selenium.webdriver.chrome.service import Service
-   options.binary_location = "/usr/bin/chromium-browser"
 
    service = Service("/usr/bin/chromedriver")
-   
 
-   driver = webdriver.Chrome(service=service,options=options)
+   driver = webdriver.Chrome(
+        service=service,
+        options=options
+    )
 
    wait = WebDriverWait(driver, 20)
 
