c69f54677516bdbc6a8785c5196102276b7b2f6d
diegogiraldoganaderia <diegogiraldo1304@gmail.com>
2026-05-28 13:03:38 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ if st.session_state.informe_nacimientos:
         if st.button("Generar informe",use_container_width=True):
             st.session_state.informe_nacimientos=False
             df_cria=buscar.ordenar_fecha_df(df_cria)
-            df_ganado_puro=buscar.ordenar_fecha_df(df_ganado_puro)
+           
             informe=buscar.informe_crias(df_cria,filtros,df_ganado_puro)
             texto="En este informe se muestran todas las crias nacidas a partir de la fecha: "+informe[0].strftime("%d/%m/%Y")+"\n"+ "cantidad de animales: "+str(len(informe[1]))
             nombre_pdf = ("Informe_Nacimientos")
