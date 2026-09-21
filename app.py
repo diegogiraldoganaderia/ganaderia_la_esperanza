df78960c0e31d335a2785f6b9797ec32bff9d68d
Diego Giraldo <diegogiraldo1304@gmail.com>
2026-09-21 12:48:47 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -79,6 +79,8 @@ st.header("MENÚ")
 #FORMULARIOS GENERALES CERRADOS
 if "informes" not in st.session_state:
     st.session_state.informes = False
+
+
 if "reproduccion_produccion" not in st.session_state:
     st.session_state.reproduccion_produccion = False
 if "registros" not in st.session_state:
@@ -117,14 +119,15 @@ if "graficos" not in st.session_state:
 if "tiempo_inseminacion" not in st.session_state:
     st.session_state.tiempo_inseminacion=False
 
-#Botones 
-#estilo de botone
-
-
+#Botones
 st.html("""
 <style>
 
-.stButton > button {
+/* =========================
+   ESTILO 1 - PRINCIPAL
+   ========================= */
+
+button[kind="primary"] {
     width: 100%;
     height: 72px !important;
 
@@ -145,50 +148,14 @@ st.html("""
     letter-spacing: 0.8px;
 
     box-shadow: 0 4px 12px rgba(0,0,0,0.25);
-
-    transition: all 0.2s ease-in-out;
-
-    padding: 0 !important;
 }
 
 
-/* Al pasar el mouse */
-.stButton > button:hover {
+/* =========================
+   ESTILO 2 - SECUNDARIO
+   ========================= */
 
-    background: linear-gradient(
-        90deg,
-        #80652B 0%,
-        #332D20 8%,
-        #1A1B1A 100%
-    ) !important;
-
-    border: 1px solid #B08D3C !important;
-
-    transform: translateY(-2px);
-
-    box-shadow: 0 7px 18px rgba(0,0,0,0.35);
-}
-
-
-/* Al presionar */
-.stButton > button:active {
-    transform: translateY(0px);
-}
-
-.stButton > button p {
-    font-size: 17px !important;
-    font-weight: 500 !important;
-    margin: 0 !important;
-}
-
-</style>
-""")
-
-def estilo():
-    st.html("""
-<style>
-
-.stButton > button {
+button[kind="secondary"] {
     width: 100%;
     height: 72px !important;
 
@@ -201,55 +168,20 @@ def estilo():
 
     color: #F2E8D0 !important;
 
-    border: 1px solid #806A35 !important;
+    border: 1px solid white !important;
     border-radius: 14px !important;
 
     font-size: 17px !important;
     font-weight: 500 !important;
-    letter-spacing: 0.8px;
 
     box-shadow: 0 4px 12px rgba(0,0,0,0.25);
-
-    transition: all 0.2s ease-in-out;
-
-    padding: 0 !important;
-}
-
-
-/* Al pasar el mouse */
-.stButton > button:hover {
-
-    background: linear-gradient(
-        90deg,
-        #80652B 0%,
-        #332D20 8%,
-        #1A1B1A 100%
-    ) !important;
-
-    border: 1px solid #B08D3C !important;
-
-    transform: translateY(-2px);
-
-    box-shadow: 0 7px 18px rgba(0,0,0,0.35);
-}
-
-
-/* Al presionar */
-.stButton > button:active {
-    transform: translateY(0px);
-}
-
-.stButton > button p {
-    font-size: 17px !important;
-    font-weight: 500 !important;
-    margin: 0 !important;
 }
 
 </style>
 """)
-#  BOTON INICIO, este botón reinicia todos los procesos
 
-if st.button("🚀  INICIO",use_container_width=True):
+
+if st.button("🚀  INICIO",use_container_width=True,type="primary"):
     st.session_state.informes = False
     st.session_state.reproduccion_produccion = False
     st.session_state.registro_nacimientos=False
@@ -267,11 +199,12 @@ if st.button("🚀  INICIO",use_container_width=True):
 
 #INICIO INFORMES
 
-if st.button("📋 INFORMES",use_container_width=True):
+
+if st.button("📋 INFORMES",use_container_width=True,type="primary"):
     st.session_state.informes= True
     
 if st.session_state.informes:
-    estilo()
+
     col1, col2, col3= st.columns(3)
     with col1:
         if st.button("INFORME NACIMIENTOS",use_container_width=True):
@@ -520,10 +453,10 @@ if st.session_state.informe_embriones:
 #-------------------------------------------------------------------------
 #INICIO REPRODUCCIÓN Y PRODUCIÓN
 
-if st.button("🤰🐄  REPRODUCCIÓN Y PRODUCCIÓN",use_container_width=True):
+if st.button("🤰🐄  REPRODUCCIÓN Y PRODUCCIÓN",use_container_width=True,type="primary"):
    st.session_state.reproduccion_produccion= True
 if st.session_state.reproduccion_produccion:
-    estilo()
+
     col1, col2, col3= st.columns(3)
     with col1:
         if st.button("REGISTRO NACIMIENTOS",use_container_width=True): 
@@ -604,10 +537,9 @@ if st.session_state.tiempo_inseminacion:
 #-------------------------------------------------------------------------
 #INICIO REGISTROS
 
-if st.button("🧬🐂  REGISTRO DE ANIMALES",use_container_width=True):
+if st.button("🧬🐂  REGISTRO DE ANIMALES",use_container_width=True,type="primary"):
     st.session_state.registros = True
-if st.session_state.registros:
-    estilo()
+if st.session_state.registros:  
     col1, col2= st.columns(2)
     with col1:
         if st.button("AÑADIR REGISTROS",use_container_width=True):
@@ -670,7 +602,7 @@ if st.session_state.buscar_registros:
 #------------------------------------------------------------------------
 #INICIO MODIFICAR BASE DE DATOS
 
-if st.button("🗃️ MODIFICAR BASE DATOS",use_container_width=True):
+if st.button("🗃️ MODIFICAR BASE DATOS",use_container_width=True,type="primary"):
         st.session_state.modificar_df = True
         st.session_state.registro_crias=False
         st.session_state.informe_nacimientos= False
@@ -721,7 +653,7 @@ if st.session_state.modificar_df:
 #------------------------------------------------------------------------
 #INICO GRACIFICOS
 
-if st.button("📊   GRÁFICOS",use_container_width=True):
+if st.button("📊   GRÁFICOS",use_container_width=True,type="primary"):
         st.session_state.graficos=True
         st.session_state.registro_crias=False
         st.session_state.informe_nacimientos= False
